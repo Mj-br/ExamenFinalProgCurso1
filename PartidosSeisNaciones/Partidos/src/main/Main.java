6070641d7c1ac0fb488f4b622dbe264d713ce01a
Manuel <ro7josemanuel@gmail.com>
2022-06-10 00:44:23 +0200

    Arreglos

---
+++ PartidosSeisNaciones/Partidos/src/main/Main.java
@@ -0,0 +1,17 @@
+package main;
+
+import java.io.IOException;
+
+import models.Competicion;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		Competicion competir = new Competicion();
+
+		competir.Menu();
+
+	}
+
+}
